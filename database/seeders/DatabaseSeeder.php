e98ecf4ac50a55ce39285d6d8eebec60a79c0d74
Anuradha <aj2740@gmail.com>
2025-12-15 18:35:16 +0530

    country,state,district,taluka seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'first_name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            CountrySeeder::class,
+            StateSeeder::class,  // if you have states
+            DistrictSeeder::class,
+            TalukaSeeder::class,
+
         ]);
     }
 }
