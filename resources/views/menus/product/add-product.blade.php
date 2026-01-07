dd69d554c7c93efff0c665b9cfd19be554c686a0
Anuradha <aj2740@gmail.com>
2026-01-07 14:59:27 +0530

    IGST renamed as gst and add calculation

--- resources/views/menus/product/add-product.blade.php
+++ resources/views/menus/product/add-product.blade.php
@@ -170,7 +170,6 @@
                                                     </div>
                                                 </div>
 
-
                                                 {{-- Prices --}}
                                                 <div class="col-md-3">
                                                     <div class="mb-3">
