b14311e4d836ff60e2db006055898ba4123a15fc
Ravi Garur <ravi.gaur@avsstech.com>
2026-01-12 15:27:51 +0530

    ui change in shop

--- resources/views/website/shop.blade.php
+++ resources/views/website/shop.blade.php
@@ -22,28 +22,29 @@
         <!-- Fruits Shop Start-->
         <div class="container-fluid fruite py-5">
             <div class="container py-5">
-                <h1 class="mb-4">Fresh fruits shop</h1>
+                <!-- <h1 class="mb-4">All Products</h1> -->
                 <div class="row g-4">
                     <div class="col-lg-12">
 
                         <div class="row g-4">
 
                             <div class="col-xl-3">
-                                <div class="input-group w-100 mx-auto d-flex">
+                                <!-- <div class="input-group w-100 mx-auto d-flex">
                                     <input type="search" class="form-control p-3" placeholder="keywords" aria-describedby="search-icon-1">
                                     <span id="search-icon-1" class="input-group-text p-3"><i class="fa fa-search"></i></span>
-                                </div>
+                                </div> -->
                             </div>
 
                             <div class="col-6"></div>
                             <div class="col-xl-3">
-                                <div class="bg-light ps-3 py-3 rounded d-flex justify-content-between mb-4">
-                                    <label for="fruits">Default Sorting:</label>
-                                    <select id="fruits" name="fruitlist" class="border-0 form-select-sm bg-light me-3" form="fruitform">
-                                        <option value="volvo">Nothing</option>
-                                        <option value="saab">Popularity</option>
-                                        <option value="opel">Organic</option>
-                                        <option value="audi">Fantastic</option>
+                                <div class="bg-light ps-3 py-3 rounded d-flex justify-content-between mb-4">                            
+                                    <select id="categoryFilter" class="form-select">
+                                        <option value="all">All Products</option>
+                                        @foreach($categories as $category)
+                                            <option value="{{ $category->id }}">
+                                                {{ $category->name }}
+                                            </option>
+                                        @endforeach
                                     </select>
                                 </div>
                             </div>
@@ -56,15 +57,7 @@
                                 <div class="row g-4">
                                     <div class="col-lg-12">
                                         <div class="mb-3">
-                                            <h4>Categories</h4>
-                                            <select id="categoryFilter" class="form-select">
-                                                <option value="all">All Products</option>
-                                                @foreach($categories as $category)
-                                                    <option value="{{ $category->id }}">
-                                                        {{ $category->name }}
-                                                    </option>
-                                                @endforeach
-                                            </select>
+
                                         </div>
                                     </div>
 
