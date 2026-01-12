fdb34f464d37da3bf996f5adeb0a95c83dd185a2
Ravi Garur <ravi.gaur@avsstech.com>
2026-01-12 10:35:26 +0530

    auth

--- routes/web.php
+++ routes/web.php
@@ -62,7 +62,7 @@ Route::post('/admin-logout', [AdminAuthController::class, 'logout'])->name('logo
 Route::post('/reset-password', [AdminAuthController::class, 'resetPassword'])
     ->name('reset.password');
 
-Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth:admin'])->group(function () {
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/products', [ProductController::class, 'index'])
@@ -960,13 +960,13 @@ Route::middleware(['web'])->group(function () {
 
 });
 
-Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
+Route::get('/enduserlogin', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/enduserlogin', [AuthController::class, 'login']);
 
-Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
-Route::post('/register', [AuthController::class, 'register']);
+Route::get('/enduserregister', [AuthController::class, 'showRegister'])->name('register');
+Route::post('/enduserregister', [AuthController::class, 'register']);
 
-Route::post('/logout', [AuthController::class, 'websitelogout'])->name('websitelogout');
+Route::post('/enduserlogout', [AuthController::class, 'websitelogout'])->name('websitelogout');
 
 Route::post('/place-order', [CheckoutController::class, 'placeOrder'])
     ->middleware('auth');
