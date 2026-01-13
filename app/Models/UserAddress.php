43cfb97ed2f370d62c000da01a3df0c6dd9f39ab
Ravi Garur <ravi.gaur@avsstech.com>
2026-01-13 17:33:46 +0530

    checkout

---
+++ app/Models/UserAddress.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserAddress extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'first_name',
+        'last_name',
+        'address',
+        'city',
+        'country',
+        'postcode',
+        'phone',
+        'email',
+    ];
+}
