355dd400bb5f94659eeee03b0fe4e36b5eb49a12
allein395 <bhaveshmane395@gmail.com>
2026-01-14 18:07:37 +0530

    Approve status chnaged to recived

--- resources/views/warehouse/index.blade.php
+++ resources/views/warehouse/index.blade.php
@@ -57,10 +57,10 @@
                             <td>{{ $t->quantity }}</td>
                             <td>{{ optional($t->created_at)->format('d-m-Y') }}</td>
                             <td>
-                             @if ($t->status == 1)
+                                        @if ($t->status == 1)
                                             <span class="badge bg-success">Dispatched</span>
                                         @elseif($t->status == 2)
-                                            <span class="badge bg-success">Approved</span>
+                                            <span class="badge bg-success">Recived</span>
                                         @else
                                             <span class="badge bg-warning">Pending</span>
                                         @endif
