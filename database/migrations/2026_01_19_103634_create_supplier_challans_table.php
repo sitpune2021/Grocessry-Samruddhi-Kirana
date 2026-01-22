48a6dd37db9591732d1f4f0963ec3e8dd0c5d469
shrutika1099 <shrutikatalokar1099@gmail.com>
2026-01-22 11:23:58 +0530

    MIGRATIONS CHANGES

--- database/migrations/2026_01_19_103634_create_supplier_challans_table.php
+++ database/migrations/2026_01_19_103634_create_supplier_challans_table.php
@@ -6,27 +6,26 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up()
     {
         Schema::create('supplier_challans', function (Blueprint $table) {
             $table->id();
+
             $table->string('challan_no')->unique();
-            $table->foreignId('purchase_order_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('supplier_id')->constrained();
-            $table->foreignId('warehouse_id')->constrained('warehouses');
+
+            $table->foreignId('supplier_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('warehouse_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('created_by')->constrained('users')->cascadeOnDelete();
+
             $table->date('challan_date');
-            $table->enum('status', ['received', 'partial', 'rejected'])->default('received');
-            $table->foreignId('created_by')->constrained('users');
+
+            $table->enum('status', ['received', 'partial', 'rejected'])
+                ->default('received');
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('supplier_challans');
